42d2859e20a362d82cba788b1ca556ef77d35416
Charles Beadle <charles.beadle@protonmail.com>
2022-07-25 16:53:06 -0400

    Init commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#ccc',
+    fontWeight: 'bold',
+    fontSize: 20
+  }
 });
